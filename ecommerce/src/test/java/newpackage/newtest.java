e98accce6d7f8ee00eb2f6c89d3fb4e77082f88b
maheshreddy1322 <45041838+maheshreddy1322@users.noreply.github.com>
2018-11-15 22:36:26 +0530

    commited

--- ecommerce/src/test/java/newpackage/newtest.java
+++ ecommerce/src/test/java/newpackage/newtest.java
@@ -5,6 +5,7 @@ public class newtest {
 	public static void main(String[] args) {
 		System.out.println("welcome to selenium");
 		System.out.println("welcome to my world");
+		System.out.println("haiiiiiiiiiiiiiii");
 
 	}
 
